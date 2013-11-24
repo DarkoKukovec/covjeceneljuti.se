7c7990216426a151d203f9c594046d07192bb446
Darko Kukovec <darko.kukovec@infinum.hr>
2013-11-24 15:34:28 +0100

    game start

--- app/scripts/routes/game.js
+++ app/scripts/routes/game.js
@@ -49,8 +49,6 @@ define([
       this.gameView.on('pawn:eat', this.goSraz, this);
       this.gameView.on('dice:throw', this.move, this);
 
-      window.game = this.game;
-
       var players = [];
       for (var i = 0; i < app.currentGame.players.length; i++) {
         var name = app.currentGame.players[i];
@@ -64,6 +62,8 @@ define([
 
       this.gameView.render();
       app.switchView(this.gameView);
+
+      this.game.start();
     },
 
     move: function() {
